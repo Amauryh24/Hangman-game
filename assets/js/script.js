d8018fca5ad04a28a0acb470b21825996bc3b854
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-21 23:06:26 +0200

    image setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
 import { dictionnaire } from "./dictionnaire.js";
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
-
 let wordToDisplay = [];
 let memoryGuessLetter = [];
 let wordToGuess;
@@ -13,8 +9,19 @@ let memoryGuessLetterText = document.getElementById("memory-guess-letter");
 let guessLetterInput = document.getElementById("guess-letter");
 let enterLabel = document.getElementById("enter-label");
 let newGameButton = document.getElementById("new-game");
+let enterButton = document.getElementById("enter-btn");
+let hangmanImage = document.getElementById("hangman-image");
+let essais;
 
-let essais = 7;
+galleryImage = [
+  "./assets/img/Hangman-0.png",
+  "./assets/img/Hangman-1.png",
+  "./assets/img/Hangman-2.png",
+  "./assets/img/Hangman-3.png",
+  "./assets/img/Hangman-4.png",
+  "./assets/img/Hangman-5.png",
+  "./assets/img/Hangman-6.png",
+];
 
 window.addEventListener("load", () => {
   initHangmanGame();
@@ -37,49 +44,85 @@ let displayWord = () => {
 };
 
 let initHangmanGame = () => {
+  memoryGuessLetter = [];
+  wordToDisplay = [];
+  essais = 0;
   randomWord();
   displayWord();
+  hangmanImage.style.display = "none";
+  guessLetterInput.style.display = "initial";
+  enterButton.style.display = "initial";
+  memoryGuessLetterText.innerText = "";
+  enterLabel.style.display = "";
+  newGameButton.style.display = "initial";
+  newGameButton.setAttribute("disabled", "");
 };
 guessLetterInput.addEventListener("keyup", (event) => {
-  event.preventDefault();
   if (event.keyCode === 13) {
-    console.log("j'ai click sur enter");
-    document.getElementById("enter-btn").click();
+    event.preventDefault();
+    enterButton.click();
   }
 });
-function guessLetter() {
-  enterLabel.innerText = "";
-  let guessLetter = guessLetterInput.value.toLowerCase();
-  guessLetterInput.value = "";
-  if (memoryGuessLetter.includes(guessLetter) === true) {
-    enterLabel.innerText = "La lettre " + guessLetter + " est déjà utilisée";
-  } else {
-    memoryGuessLetter.push(guessLetter);
-    memoryGuessLetterText.innerText = memoryGuessLetter.join(", ");
-  }
+enterButton.addEventListener("click", () => {
+  guessLetter();
+});
 
-  if (wordToGuess.includes(guessLetter) === true) {
-    for (let i = 0; i < wordToGuess.length; i++) {
-      if (guessLetter === wordToGuess[i]) {
-        wordToDisplay[i] = guessLetter;
-        wordToDisplayText.innerText = wordToDisplay.join(" ");
+let guessLetter = () => {
+  enterLabel.innerText = "";
+  let reg = /[a-z]/g;
+  let guessLetter = guessLetterInput.value.toLowerCase();
+
+  let result = guessLetter.match(reg);
+  if (result === null) {
+    enterLabel.innerText =
+      "Pas d'espace, pas d'accent, pas de numéro, uniquement les lettes de A à Z";
+  } else {
+    if (memoryGuessLetter.includes(guessLetter) === true) {
+      enterLabel.innerText = "La lettre " + guessLetter + " est déjà utilisée";
+    } else {
+      memoryGuessLetter.push(guessLetter);
+      memoryGuessLetterText.innerText = memoryGuessLetter.join(", ");
+
+      if (wordToGuess.includes(guessLetter) === true) {
+        for (let i = 0; i < wordToGuess.length; i++) {
+          if (guessLetter === wordToGuess[i]) {
+            wordToDisplay[i] = guessLetter;
+            wordToDisplayText.innerText = wordToDisplay.join(" ");
+          }
+        }
+        if (wordToGuess.join("") === wordToDisplay.join("")) {
+          alert("Bravo, vous avez gagné");
+          playAgain();
+        }
+      } else {
+        essais++;
+        if (essais < 7) {
+          alert(
+            "Vous avez perdu, le mot à trouvé était: " + wordToGuess.join("")
+          );
+          playAgain();
+        }
       }
     }
-  } else {
-    essais--;
-    if (essais == 0) {
-      alert("Vous avez perdu, le mot à trouvé était: " + wordToGuess.join(""));
-      let answer = "voulez vous rejouez ?";
-      if (answer === true) initHangmanGame();
-    }
   }
-
-  if (wordToGuess.join("") === wordToDisplay.join("")) {
-    alert("fini");
-    newGameButton.removeAttribute("disabled", "");
-  }
-}
+  guessLetterInput.value = "";
+};
 
 newGameButton.addEventListener("click", () => {
   initHangmanGame();
 });
+
+let playAgain = () => {
+  let answer = confirm("voulez vous rejouez ?");
+  answer === true ? initHangmanGame() : quitGame();
+};
+
+let quitGame = () => {
+  wordToDisplayText.innerText = "";
+  guessLetterInput.style.display = "none";
+  enterButton.style.display = "none";
+  enterLabel.style.display = "none";
+  memoryGuessLetterText.innerText = "";
+  newGameButton.style.display = "initial";
+  newGameButton.removeAttribute("disabled", "");
+};
